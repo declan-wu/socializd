114936dfb5b797a4e75516a75922e89232fb6c78
Declan Wu <declan.s.wu@gmail.com>
2019-08-23 16:24:17 -0400

    modified routers, not finished yet

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,7 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
+const express = require("express");
+const router = express.Router();
 
-const express = require('express');
-const router  = express.Router();
-
-module.exports = (db) => {
+module.exports = db => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
       .then(data => {
@@ -16,9 +9,7 @@ module.exports = (db) => {
         res.json({ users });
       })
       .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+        res.status(500).json({ error: err.message });
       });
   });
   return router;

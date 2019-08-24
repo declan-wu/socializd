f0d3ab9980aa4a528908984844be2926edb53966
Annie <anniekao@posteo.net>
2019-08-24 12:00:40 -0400

    Feat/ poll.ejs added, Modified app.js, Added footer and header partials

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,43 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });
+
+  // Jquery for the initial polling form
+  //
+  // Counter to keep track of ids of textareas
+  let optionId = 3;
+
+  $('form').on('keyup', ".form__option", function(event) {
+    if (event.which === 13 && optionId <= 5) {
+      $("form").find('.form__email').before(
+        $('<input />').attr('id', 'option' + optionId.toString()).attr('type', 'textarea').attr('placeholder', 'Option').addClass('form__option')
+      );
+      optionId++;
     }
-  });;
+  });
+
+  // give the submit / reset buttons the effect of being pressed
+  // down when clicked
+  const clicked = (target) => {
+    $(target).addClass('clicked').delay(200).queue(function() {
+      $(target).removeClass('clicked').dequeue();
+    });
+  };
+
+  $('.form__reset').on('click', function(event) {
+    clicked(this);
+    // TODO: Be able to remove text boxes that have been added on
+    $('.form__option').val('');
+    $('.form__title').val('');
+  });
+
+  $('.form__submit').on('click', function(event) {
+    clicked(this);
+  });
 });

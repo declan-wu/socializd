11954ddefbb08b5015029c3102d50441b5921b0a
Declan Wu <declan.s.wu@gmail.com>
2019-08-25 15:32:10 -0400

    feat: finished regular bar chart, need to fix the ejs, done result routes

--- routes/result.js
+++ routes/result.js
@@ -10,18 +10,22 @@ module.exports = db => {
     SELECT options.name, SUM(rankings.relative_points) AS total_points
     FROM rankings
     JOIN options ON rankings.option_id = options.id
-    WHERE options.poll_id = $1
+    WHERE rankings.poll_id = $1
     GROUP BY options.name
     ORDER BY SUM(rankings.relative_points) DESC;`;
 
     try {
       const data = await db.query(query_string, query_params);
-      const ret = {};
-      console.log(data.rows);
+      const ret = [];
+      // console.log(data.rows);
       for (let row of data.rows) {
-        ret[row.name] = row.total_points;
+        ret.push([row.name, Number(row.total_points)]);
       }
-      res.render("result", ret);
+      console.log(ret);
+      temp_var = { what: ret };
+      res.render("result", temp_var);
+
+      // res.render("result", { option_1: [10, 10, 1], option_2: 4, option_3: 5 });
     } catch (e) {
       console.error(e);
     }

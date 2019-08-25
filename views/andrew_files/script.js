65c3ebd9ab42969f5eb07d2cfab509d51e68522d
Declan Wu <declan.s.wu@gmail.com>
2019-08-25 13:26:33 -0400

    added andrews files

---
+++ views/andrew_files/script.js
@@ -0,0 +1,25 @@
+//reveals next button uppon entering text
+$('.form-control').keyup(function () {
+    $('.nxt').removeClass("hide fadeOutDown").addClass("fadeInUp");
+})
+
+//on click (next), 
+$('.nxt').click(function () {
+    $('.nxt').removeClass("fdeInUp").addClass('fadeOutDown');
+    if ($('.progress-form li').hasClass('activate')) {
+        var $activate = $('li.activate');
+        var $inactive = $('li.inactive');
+        $activate.removeClass("fadeInRightBig activate").addClass('fadeOutLeftBig');
+        $inactive.removeClass("hide inactive").addClass("activate fadeInRightBig").next().addClass('inactive');
+        $('.submit').click(function () {
+            event.preventDefault();
+            var darken = '<div class="darken" style="display:none;"></div>';
+            $('body').prepend(darken);
+            $(".darken").delay().show(0).animate({
+                opacity: 0.8
+            }, "fast");
+            $('.thanks').removeClass('hide').addClass('fadeInDownBig');
+        });
+    }
+});
+

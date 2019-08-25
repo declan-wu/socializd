1cb8d7f2e3d36792f4362785e184953850f00b36
Declan Wu <declan.s.wu@gmail.com>
2019-08-25 13:21:58 -0400

    modified index.js

--- routes/index.js
+++ routes/index.js
@@ -42,13 +42,9 @@ module.exports = db => {
           q_arr.push(`( ${poll_id}, $${option_params.length} )`);
           console.log("options: ", option_params);
           console.log("option params length: ", option_params.length);
-          // (15, )
         }
         option_string += q_arr.join(", ");
         option_string += ";";
-
-        console.log("option params: ", option_params);
-        console.log("option string: ", option_string);
 
         // insert into options (poll_id, name) VALUES (1, 'name'), (1, 'name')
         db.query(option_string, option_params);

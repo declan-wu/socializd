03448c1f7e2f99fbb20b3f41403a44be5520f915
Andrew Ting <andrewting112@gmail.com>
2019-08-26 22:12:26 +0000

    added poll title and created date on donut chart

--- routes/result.js
+++ routes/result.js
@@ -14,15 +14,27 @@ module.exports = db => {
     GROUP BY options.name
     ORDER BY SUM(rankings.relative_points) DESC;`;
 
+    const query_date_title = `
+    SELECT created_date, title
+    FROM polls
+    WHERE polls.id = $1;
+    `;
+
     try {
-      const data = await db.query(query_string, query_params);
-      const ret = [];
-      // console.log(data.rows);
-      for (let row of data.rows) {
-        ret.push([row.name, Number(row.total_points)]);
+      const data_date_title = await db.query(query_date_title, query_params);
+      const options_data = await db.query(query_string, query_params);
+      const options = [];
+      const created_date = data_date_title.rows[0].created_date;
+      const poll_title = data_date_title.rows[0].title;
+      // console.log(data_date_title.rows[0].created_date);
+      // console.log(data_date_title.rows[0].title);
+
+
+      for (let row of options_data.rows) {
+        options.push([row.name, Number(row.total_points)]);
       }
       // console.log(ret);
-      temp_var = { what: ret };
+      temp_var = { what: options, created_date: created_date, poll_title: poll_title };
       res.render("resultcopy", temp_var);
 
       // res.render("result", { option_1: [10, 10, 1], option_2: 4, option_3: 5 });

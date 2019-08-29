c2a180404de25c0bcd458fe2ecc008584a0bb593
Declan Wu <declan.s.wu@gmail.com>
2019-08-29 15:11:25 -0400

    modified dashboard js for jquery and ajax

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,35 +1,54 @@
 $(() => {
-  $('a').on('click', function(event) {
+  $("a").on("click", function(event) {
+    event.preventDefault();
     const pollId = event.target.id;
-    $('.close').show();
-    $('.view-btn').show();
-    $('.delete-btn').show();
-    $(".view-submit")
-      .attr("action", `/result/${pollId}`)
-      .attr("method", "GET");
+    $(".close").show();
+    $(".view-btn").show();
+    $(".delete-btn").show();
+    // $(".view-submit")
+    //   .attr("action", `/result/${pollId}`)
+    //   .attr("method", "GET");
 
-    $.ajax({
-      type: "GET",
-      url: `/result/${pollId}`,
-      success: function(data) {
-        console.log(data);
+    $.post(`/dashboard/${pollId}`, options => {
+      let donutData = {
+        data: []
+      };
+      options.forEach(([name, quantity]) => {
+        donutData.data.push({ name, quantity });
+      });
+
+      function createDonutChart() {
+        let donutChart = britecharts.donut(),
+          donutContainer = d3.select(".js-donut-chart-container"),
+          containerWidth = donutContainer.node()
+            ? donutContainer.node().getBoundingClientRect().width
+            : false;
+        donutChart
+          .isAnimated(true)
+          .highlightSliceById(2)
+          .hasFixedHighlightedSlice(true)
+          .width(containerWidth)
+          .height(containerWidth)
+          .externalRadius(containerWidth / 2.5)
+          .internalRadius(containerWidth / 5)
+          .on("customMouseOver", function(data) {
+            legendChart.highlight(data.data.id);
+          })
+          .on("customMouseOut", function() {
+            legendChart.clearHighlight();
+          });
+        donutContainer.datum(donutData.data).call(donutChart);
+        donutChart.highlightSliceById(3).isAnimated(true); //not working
+        donutContainer.datum(donutData.data).call(donutChart);
       }
+      createDonutChart();
     });
- 
-    // $(".donut").load(`result/${pollId}`, function() {
-    //   $(".header-result").remove();
-    //   $(".head").remove();
-    //   $(".legend-container-group").remove();
-    //   $(".js-donut-chart-container").addClass('loaded-donut');
-    // });
-
-
   });
 
-  $('.close').on('click', function(event) {
+  $(".close").on("click", function(event) {
     $(this).hide();
-    $('.view-btn').hide();
-    $('.delete-btn').hide();
+    $(".view-btn").hide();
+    $(".delete-btn").hide();
     $(".donut").empty();
   });
 });
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const moment = require("moment");
 
 module.exports = db => {
-  router.get('/', async (req, res) => {
+  router.get("/", async (req, res) => {
     // const userId = req.session.userId;
     const userId = 1;
 
@@ -14,7 +14,7 @@ module.exports = db => {
       WHERE users.id = $1;
     `;
     const user_query_params = [userId];
-    
+
     try {
       const userPollData = await db.query(user_query_string, user_query_params);
       const username = userPollData.rows[0].name;
@@ -38,7 +38,7 @@ module.exports = db => {
 
       for (let i = 0; i < pollIds.length; i++) {
         if (i === 0) {
-          options_ranking_string += '(' + `$${i + 1}, `;
+          options_ranking_string += "(" + `$${i + 1}, `;
         } else if (i === pollIds.length - 1) {
           options_ranking_string += `$${i + 1})`;
         } else {
@@ -50,20 +50,29 @@ module.exports = db => {
         GROUP BY polls.id, options.name
         ORDER BY SUM(rankings.relative_points) DESC;`;
 
-      const optionsRankingData = await db.query(options_ranking_string, pollIds);
+      const optionsRankingData = await db.query(
+        options_ranking_string,
+        pollIds
+      );
       const optionsRankings = optionsRankingData.rows;
-      
+
       const options = {};
       for (let option of optionsRankings) {
         if (options[option.poll_id]) {
-          options[option.poll_id].push([option.name, Number(option.total_points)]);
+          options[option.poll_id].push([
+            option.name,
+            Number(option.total_points)
+          ]);
         } else {
           options[option.poll_id] = [];
-          options[option.poll_id].push([option.name,Number(option.total_points)]);
+          options[option.poll_id].push([
+            option.name,
+            Number(option.total_points)
+          ]);
         }
       }
       console.log(options);
-      
+
       const renderVars = {
         username,
         pollIds,
@@ -74,6 +83,29 @@ module.exports = db => {
       res.render("dashboard", renderVars);
     } catch (err) {
       console.error(err);
+    }
+  });
+
+  router.post("/:id", async (req, res) => {
+    const pollId = req.params.id;
+    const query_params = [pollId];
+    const query_string = `
+    SELECT options.name, SUM(rankings.relative_points) AS total_points
+    FROM rankings
+    JOIN options ON rankings.option_id = options.id
+    WHERE rankings.poll_id = $1
+    GROUP BY options.name
+    ORDER BY SUM(rankings.relative_points) DESC;`;
+    try {
+      const options_data = await db.query(query_string, query_params);
+      const options = [];
+
+      for (let row of options_data.rows) {
+        options.push([row.name, Number(row.total_points)]);
+      }
+      res.send({ options });
+    } catch (e) {
+      console.log(e);
     }
   });
 

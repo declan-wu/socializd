6a7e970242d00c589b80c77fd14341c3f91515f9
Annie <anniekao@posteo.net>
2019-08-28 20:25:04 -0400

    Added view and delete buttons and link to add emails(non-functional)

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,10 +1,22 @@
 $(() => {
   $('a').on('click', function(event) {
     const pollId = event.target.id;
+    $('.close').show();
+    $('.view-btn').show();
+    $('.delete-btn').show();
+    $(".view-submit")
+      .attr("action", `/result/${pollId}`)
+      .attr("method", "GET");
     $(".donut").load(`result/${pollId}`, function() {
       $(".header-result").remove();
       $(".head").remove();
       $(".legend-container-group").remove();
     });
+  });
+
+  $('.close').on('click', function(event) {
+    $(this).hide();
+    $('.view-btn').hide();
+    $(".donut").empty();
   });
 });
